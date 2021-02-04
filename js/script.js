c172a366af5d1beab41e1efeabb65d261101953e
Vs-mydevelopmens <73613008+Vs-mydevelopmens@users.noreply.github.com>
2021-02-04 21:51:01 +0100

    new

--- js/script.js
+++ js/script.js
@@ -243,8 +243,6 @@ var $tabs = function (target) {
 };
 
 $tabs('.tabs');
-
-
 // BildSlider
 let sliders = document.querySelectorAll('._swiper');
 if (sliders) {
@@ -271,7 +269,6 @@ if (sliders) {
 		}
 	}
 	slider_bild_callback();
-
 }
 
 function slider_bild_callback(params) { }
@@ -348,10 +345,10 @@ $(document).ready(
 			observer: true,
 			observeParents: true,
 			// effect: 'fade',
-			autoplay: {
-				delay: 3000,
-				disableOnInteraction: false,
-			},
+			// autoplay: {
+			// 	delay: 3000,
+			// 	disableOnInteraction: false,
+			// },
 			slidesPerView: 3,
 			spaceBetween: 30,
 			// autoHeight: true,
@@ -406,43 +403,6 @@ $(document).ready(
 	});
 
 
-
-const animItems = document.querySelectorAll('._anim-items');
-
-if (animItems.length > 0) {
-	window.addEventListener('scroll', animOnScroll);
-	function animOnScroll() {
-		for (let index = 0; index < animItems.length; index++) {
-			const animItem = animItems[index];
-			const animItemHeight = animItem.offsetHeight;
-			const animItemOffset = offset(animItem).top;
-			const animStart = 6;
-
-			let animItemPoint = window.innerHeight - animItemHeight / animStart;
-			if (animItemHeight > window.innerHeight) {
-				animItemPoint = window.innerHeight - window.innerHeight / animStart;
-			}
-
-			if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
-				animItem.classList.add('scroll_active');
-			} else {
-				if (!animItem.classList.contains('_anim-no-hide')) {
-					animItem.classList.remove('scroll_active');
-				}
-			}
-		}
-	}
-	function offset(el) {
-		const rect = el.getBoundingClientRect(),
-			scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-		return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
-	}
-
-	setTimeout(() => {
-		animOnScroll();
-	}, 300);
-}
 
 // ==========================================
 // <КАРТОЧКА КОРЗИНЫ ШАПКИ САЙТА>
